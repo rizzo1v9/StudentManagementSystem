3fc2d13b638dfb33e0e957e59713ad5b1c56c3a9
carlorizzo <cssanctuary1@gmail.com>
2022-03-19 23:35:01 -0400

    Database functionality working, adding comments

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
+// Basic student class which will prompt the user to enter in their details
+
 public class Student {
+
+    private int studentID = 0;
     private String firstName;
     private String lastName;
     private String email;
-    private int studentID;
+    private String emailSnip;
     private int numOfCourses;
-    private int balance;
+    private double balance;
 
-    public Student(String firstName, String lastName, String email, int studentID, int numOfCourses, int balance){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.studentID = studentID;
-        this.numOfCourses = numOfCourses;
-        this.balance = balance;
+    // The constructor will create the format required to generate unique email addresses for every student
+    // It will also generate a mock balance of 2500 per class enrolled in
+
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the student's first name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter the student's last name: ");
+        this.lastName = in.nextLine();
+        this.email = Character.toLowerCase(firstName.charAt(0)) + lastName.toLowerCase() + "@school.edu";
+        this.emailSnip = Character.toLowerCase(firstName.charAt(0)) + lastName.toLowerCase();
+        System.out.println("Enter the number of courses the student is taking: ");
+        this.numOfCourses = Integer.parseInt(in.nextLine());
+        this.balance = numOfCourses * 2500;
+        System.out.println("Press Q to quit");
+    }
+
+    // Getters and Setters
+
+    public void setStudentID(int newStudentID){
+        this.studentID = newStudentID;
+    }
+
+    public int getStudentID(){
+        return this.studentID;
     }
 
     public void setFirstName(String newFirstName){
@@ -27,20 +51,24 @@ public class Student {
         this.lastName = newLastName;
     }
 
-    public void setEmail(String firstName, String lastName){
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public void setEmail(String firstName, String lastName, int count){
         this.email = Character.toLowerCase(firstName.charAt(0)) + lastName.toLowerCase() + "@school.edu";
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return this.email;
     }
 
-    public void setStudentID(int newStudentID){
-        this.studentID = newStudentID;
+    public String getEmailSnip(){
+        return this.emailSnip;
     }
 
-    public int getStudentID(){
-        return this.studentID;
+    public void setEmailSnip(){
+        this.emailSnip = Character.toLowerCase(firstName.charAt(0)) + lastName.toLowerCase();
     }
 
     public void setNumOfCourses(int newNumOfCourses){
@@ -55,20 +83,14 @@ public class Student {
         this.balance = numOfCourses * 2500;
     }
 
-    public int getBalance(){
+    public double getBalance(){
         return this.balance;
     }
 
-    public void makePayment(int balance, int payment){
+    // Method so the student can make a payment
+
+    public void makePayment(double balance, double payment){
         this.balance -= payment;
     }
 
-    public static void main(String[] args) {
-        Student s = new Student("Carlo", "Rizzo", "", 0, 4, 0);
-        s.setEmail(s.firstName, s.lastName);
-        System.out.println(s.email);
-        s.setBalance(s.numOfCourses);
-        s.makePayment(s.balance, 500);
-        System.out.println(s.getBalance());
-    }
 }
